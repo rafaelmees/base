f1b5d1033fe1a217a217cb5f1c2f75cc79a2fd7d
Rai Vieira <rai.vieira@bludata.com.br>
2016-06-13 09:40:03 -0300

    alteracao em BaseTest

--- src/Tests/BaseTest.php
+++ src/Tests/BaseTest.php
@@ -2,7 +2,9 @@
 
 namespace Bludata\Tests;
 
+use Auth;
 use Faker\Factory;
+use Illuminate\Auth\GenericUser;
 use Laravel\Lumen\Testing\TestCase;
 
 abstract class BaseTest extends TestCase
@@ -14,7 +16,7 @@ abstract class BaseTest extends TestCase
      */
     public function createApplication()
     {
-        
+
     }
 
     public function getFaker($locale = 'pt_BR')
