03c5681b6c327b0f770cd865c2423ec34b551283
raivieira <rai.vieira@bludata.com.br>
2016-08-18 10:39:34 -0400

    Applied fixes from StyleCI

--- src/Helpers/FormatHelper.php
+++ src/Helpers/FormatHelper.php
@@ -11,12 +11,9 @@ class FormatHelper
 
     public static function parseDate($date, $from = 'yyyy-mm-dd', $to = 'obj')
     {
-        if (is_string($date) && $from == 'yyyy-mm-dd' && $to == 'obj')
-        {
+        if (is_string($date) && $from == 'yyyy-mm-dd' && $to == 'obj') {
             return new \DateTime($date);
-        }
-        elseif (is_string($date) && $from == 'dd/mm/yyyy' && ($to == 'obj' || $to == 'yyyy-mm-dd'))
-        {
+        } elseif (is_string($date) && $from == 'dd/mm/yyyy' && ($to == 'obj' || $to == 'yyyy-mm-dd')) {
             $explode = explode('/', $date);
 
             $date = $explode[2].'-'.$explode[1].'-'.$explode[0];
