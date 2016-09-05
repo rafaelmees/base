377808feaa571ce5dd0a53f961d05ec48e5613bd
Jonathan A. Schweder <jonathanschweder@gmail.com>
2016-09-05 16:02:04 -0300

    criado módulo para trabalhar com Soap

---
+++ src/Soap/SoapClient.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Bludata\Soap;
+
+use Exception;
+use SoapClient as NativeSoapClient;
+use SoapFault;
+
+class SoapClient
+{
+    /**
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @var SoapClient
+     */
+    protected $client;
+
+    /**
+     * @var mixed
+     */
+    protected $service;
+
+    /**
+     * @var mixed
+     */
+    protected $request;
+
+    /**
+     * Cria um instancia para comunicação com WSDL utilizando SoapClient.
+     *
+     * @param string $host    Parametro obrigatório, endereço do WSDL
+     * @param array  $options Parametro opcional, referente aos options que serão utilizados
+     */
+    public function __construct($host, array $options = [])
+    {
+        $this->setHost($host)
+            ->setOptions($options);
+    }
+
+    /**
+     * Efetua a conexão inicial com o WSDL.
+     *
+     * @return self
+     */
+    public function connect()
+    {
+        if (!$host = $this->getHost()) {
+            throw new Exception('Host não informado');
+        }
+
+        $client = new NativeSoapClient($host, $this->getOptions());
+        $this->setClient($client);
+        return $this;
+    }
+
+    /**
+     * Faz a chamada para o WSDL, de acordo com os parametros pré-configurados para o serviço.
+     *
+     * @return SoapClient::__soapCall
+     */
+    public function call()
+    {
+        if (!$this->getHost()) {
+            throw new Exception('HOST do WSDL não foi informado');
+        }
+
+        if (!$this->getService()) {
+            throw new Exception('Serviço não informado');
+        }
+
+        if (!$this->getRequest()) {
+            throw new Exception('Request não informado');
+        }
+
+        if (!$this->getClient()) {
+            if (!$this->connect()) {
+                throw new Exception('Não foi possivel conectar ao WSDL');
+            }
+        }
+
+        try {
+            $client = $this->getClient();
+            return $client->__soapCall($this->getService(), $this->getRequest());
+        } catch (SoapFault $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Retorna o host do WSDL.
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * Recebe o endereço do WSDL.
+     *
+     * @param string $host
+     *
+     * @return self
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * Retorna os options utilizados no SoapClient.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Recebe os options que serão utilizados no SoapClient.
+     *
+     * @param array $options
+     *
+     * @return self
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    /**
+     * Retorna dados da requisição.
+     *
+     * @return mixed
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Recebe dados para montar o request.
+     *
+     * @param array
+     *
+     * @return self
+     */
+    public function setRequest($params)
+    {
+        if (!$request = $this->getRequest()) {
+            $request = [];
+        }
+
+        foreach ($params as $param => $value) {
+            $request[$param] = $value;
+        };
+
+        $this->request = $request;
+
+        return $this;
+    }
+
+    /**
+     * Retorna o serviço utilizado.
+     *
+     * @return mixed
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    /**
+     * Recebe o serviço que sera utilizado no WSDL.
+     *
+     * @param string $service
+     *
+     * @return self
+     */
+    public function setService($service)
+    {
+        $this->service = $service;
+        return $this;
+    }
+
+    /**
+     * Retorna a instancia do SoapClient que foi incializada.
+     *
+     * @return mixed
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Recebe o SoapClient que sera utilizado.
+     *
+     * @param SoapClient $client
+     *
+     * @return self
+     */
+    public function setClient(NativeSoapClient $client)
+    {
+        $this->client = $client;
+        return $this;
+    }
+}
---
+++ tests/Soap/SoapClientTest.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Bludata\Tests\Soap;
+
+use SoapClient as NativeSoapClient;
+use Bludata\Tests\TestCase;
+use Bludata\Soap\SoapClient;
+
+class SoapClientTest extends TestCase
+{
+    public function clients()
+    {
+        $wsdls = [
+            'https://portalhomolog.detran.go.gov.br/sah/WSHabilitacaoSimulador?wsdl',
+        ];
+        $clients = collect();
+        foreach ($wsdls as $wsdl) {
+            $clients->push(new SoapClient($wsdl));
+        }
+        return $clients;
+    }
+
+    public function testIsInstanciable()
+    {
+        $this->assertTrue(
+            class_exists(SoapClient::class),
+            'classe "Bludata\Soap\SoapClient" não encontrada'
+        );
+
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertInstanceOf(SoapClient::class, $client);
+        });
+        return $clients;
+    }
+
+    public function testConnect()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client){
+            $this->assertInstanceOf(SoapClient::class, $client->connect());
+        });
+    }
+
+    public function testSetHost()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'setHost'), 'método "setHost" não encontrado');
+            $client->setHost('http://example.com');
+            $this->assertObjectHasAttribute('host', $client);
+            $this->assertAttributeEquals('http://example.com', 'host', $client);
+        });
+    }
+
+    /**
+     * @depends testSetHost
+     */
+    public function testGetHost()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'getHost'), 'método "getHost" não encontrado');
+            $host = $client->getHost();
+            $this->assertNotEmpty($host);
+        });
+    }
+
+    public function testSetOptions()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'setOptions'), 'método "setOptions" não encontrado');
+            $client->setOptions(['this' => 'is a test']);
+            $this->assertObjectHasAttribute('options', $client);
+            $this->assertAttributeEquals(['this' => 'is a test'], 'options', $client);
+        });
+    }
+
+    /**
+     * @depends testSetOptions
+     */
+    public function testGetOptions()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'getOptions'), 'método "getOptions" não encontrado');
+            $client->setOptions(['this' => 'is another test']);
+            $options = $client->getOptions();
+            $this->assertNotEmpty($options);
+        });
+    }
+
+    public function testSetRequest()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'setRequest'), 'método "setRequest" não encontrado');
+            $client->setRequest(['this' => 'is a request test']);
+            $this->assertObjectHasAttribute('request', $client);
+            $this->assertAttributeEquals(['this' => 'is a request test'], 'request', $client);
+        });
+    }
+
+    /**
+     * @depends testSetRequest
+     */
+    public function testGetRequest()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'getRequest'), 'método "getRequest" não encontrado');
+            $client->setRequest(['this' => 'is another request test']);
+            $request = $client->getRequest();
+            $this->assertNotEmpty($request);
+        });
+    }
+
+    public function testSetService()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'setService'), 'método "setService" não encontrado');
+            $client->setService('someService');
+            $this->assertObjectHasAttribute('service', $client);
+            $this->assertAttributeEquals('someService', 'service', $client);
+        });
+    }
+
+    /**
+     * @depends testSetService
+     */
+    public function testGetService()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'getService'), 'método "getService" não encontrado');
+            $client->setService('someService');
+            $service = $client->getService();
+            $this->assertNotEmpty($service);
+        });
+    }
+
+    public function testSetClient()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'setClient'), 'método "setClient" não encontrado');
+            $nativeCliente = new NativeSoapClient($client->getHost());
+            $client->setClient($nativeCliente);
+            $this->assertObjectHasAttribute('client', $client);
+            $this->assertAttributeEquals($nativeCliente, 'client', $client);
+        });
+    }
+
+    /**
+     * @depends testSetClient
+     */
+    public function testGetClient()
+    {
+        $clients = $this->clients();
+        $clients->each(function($client) {
+            $this->assertTrue(method_exists($client, 'getClient'), 'método "getClient" não encontrado');
+            $nativeCliente = new NativeSoapClient($client->getHost());
+            $client->setClient($nativeCliente);
+            $nativeClient = $client->getClient();
+            $this->assertNotEmpty($nativeClient);
+        });
+    }
+}
