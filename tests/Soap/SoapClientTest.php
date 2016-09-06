3e42d33fa0482bf07379151cd7c37d508bba385a
Jonathan A. Schweder <jonathanschweder@gmail.com>
2016-09-06 08:57:48 -0300

    feito melhorias nos testes do SoapClient

--- tests/Soap/SoapClientTest.php
+++ tests/Soap/SoapClientTest.php
@@ -10,225 +10,205 @@ class SoapClientTest extends TestCase
 {
     public function clients()
     {
-        $wsdls = [
-            'https://portalhomolog.detran.go.gov.br/sah/WSHabilitacaoSimulador?wsdl'
+        // WS aberto do Detran de Goiás
+        $wsDetranGO = new SoapClient('https://portalhomolog.detran.go.gov.br/sah/WSHabilitacaoSimulador?wsdl');
+        $wsDetranGO->setService('retornarAulasCandidato');
+        $wsDetranGO->setRequest(['aluno' => 'anyone']);
+
+        return [
+            [$wsDetranGO]
         ];
-        $clients = collect();
-        foreach ($wsdls as $wsdl) {
-            $clients->push(new SoapClient($wsdl));
-        }
-        return $clients;
     }
 
-    public function testIsInstanciable()
+    /**
+     * @dataProvider clients
+     */
+    public function testIsInstanciable($client)
     {
         $this->assertTrue(
             class_exists(SoapClient::class),
             'classe "Bludata\Soap\SoapClient" não encontrada'
         );
 
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertInstanceOf(SoapClient::class, $client);
-        });
-        return $clients;
+        $this->assertInstanceOf(SoapClient::class, $client);
+        return $client;
     }
 
     /**
+     * @dataProvider clients
      * @expectedException        InvalidArgumentException
      * @expectedExceptionMessage Host não informado
      */
-    public function testCallWithoutHost()
+    public function testCallWithoutHost($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $client->setHost('');
-            $client->call();
-        });
+        $client->setHost('');
+        $client->call();
     }
 
     /**
+     * @dataProvider clients
      * @expectedException        InvalidArgumentException
      * @expectedExceptionMessage Serviço não informado
      */
-    public function testCallWithoutService()
+    public function testCallWithoutService($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $client->setService('');
-            $client->call();
-        });
+        $client->setService('');
+        $client->call();
     }
 
     /**
+     * @dataProvider clients
      * @expectedException        InvalidArgumentException
      * @expectedExceptionMessage Request não informada
      */
-    public function testCallWithoutRequest()
+    public function testCallWithoutRequest($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $client->setService('retornarAulasCandidato');
-            $client->setRequest([]);
-            $client->call();
-        });
-    }
-
-    public function testCall()
-    {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $client->setService('retornarAulasCandidato');
-            $client->setRequest(['aluno' => 'anyone']);
-            $response = $client->call();
-            $this->assertObjectHasAttribute('return', $response);
-        });
-    }
-
-    public function testConnect()
-    {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertInstanceOf(SoapClient::class, $client->connect());
-        });
+        $client->setService('retornarAulasCandidato');
+        $client->setRequest([]);
+        $client->call();
     }
 
     /**
+     * @dataProvider clients
+     */
+    public function testCall($client)
+    {
+        $response = $client->call();
+        $this->assertObjectHasAttribute('return', $response);
+    }
+
+    /**
+     * @dataProvider clients
+     */
+    public function testConnect($client)
+    {
+        $this->assertInstanceOf(SoapClient::class, $client->connect());
+    }
+
+    /**
+     * @dataProvider clients
      * @expectedException        InvalidArgumentException
      * @expectedExceptionMessage Host não informado
      */
-    public function testConnectWithoutPassingHost()
+    public function testConnectWithoutPassingHost($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $client->setHost('');
-            $client->connect();
-        });
-    }
-
-    public function testSetHost()
-    {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'setHost'), 'método "setHost" não encontrado');
-            $client->setHost('http://example.com');
-            $this->assertObjectHasAttribute('host', $client);
-            $this->assertAttributeEquals('http://example.com', 'host', $client);
-        });
+        $client->setHost('');
+        $client->connect();
     }
 
     /**
+     * @dataProvider clients
+     */
+    public function testSetHost($client)
+    {
+        $this->assertTrue(method_exists($client, 'setHost'), 'método "setHost" não encontrado');
+        $client->setHost('http://example.com');
+        $this->assertObjectHasAttribute('host', $client);
+        $this->assertAttributeEquals('http://example.com', 'host', $client);
+    }
+
+    /**
+     * @dataProvider clients
      * @depends testSetHost
      */
-    public function testGetHost()
+    public function testGetHost($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'getHost'), 'método "getHost" não encontrado');
-            $host = $client->getHost();
-            $this->assertNotEmpty($host);
-        });
-    }
-
-    public function testSetOptions()
-    {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'setOptions'), 'método "setOptions" não encontrado');
-            $client->setOptions(['this' => 'is a test']);
-            $this->assertObjectHasAttribute('options', $client);
-            $this->assertAttributeEquals(['this' => 'is a test'], 'options', $client);
-        });
+        $this->assertTrue(method_exists($client, 'getHost'), 'método "getHost" não encontrado');
+        $host = $client->getHost();
+        $this->assertNotEmpty($host);
     }
 
     /**
+     * @dataProvider clients
+     */
+    public function testSetOptions($client)
+    {
+        $this->assertTrue(method_exists($client, 'setOptions'), 'método "setOptions" não encontrado');
+        $client->setOptions(['this' => 'is a test']);
+        $this->assertObjectHasAttribute('options', $client);
+        $this->assertAttributeEquals(['this' => 'is a test'], 'options', $client);
+    }
+
+    /**
+     * @dataProvider clients
      * @depends testSetOptions
      */
-    public function testGetOptions()
+    public function testGetOptions($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'getOptions'), 'método "getOptions" não encontrado');
-            $client->setOptions(['this' => 'is another test']);
-            $options = $client->getOptions();
-            $this->assertNotEmpty($options);
-        });
-    }
-
-    public function testSetRequest()
-    {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'setRequest'), 'método "setRequest" não encontrado');
-            $client->setRequest(['this' => 'is a request test']);
-            $this->assertObjectHasAttribute('request', $client);
-            $this->assertAttributeEquals(['this' => 'is a request test'], 'request', $client);
-        });
+        $this->assertTrue(method_exists($client, 'getOptions'), 'método "getOptions" não encontrado');
+        $client->setOptions(['this' => 'is another test']);
+        $options = $client->getOptions();
+        $this->assertNotEmpty($options);
     }
 
     /**
+     * @dataProvider clients
+     */
+    public function testSetRequest($client)
+    {
+        $this->assertTrue(method_exists($client, 'setRequest'), 'método "setRequest" não encontrado');
+        $client->setRequest(['this' => 'is a request test']);
+        $this->assertObjectHasAttribute('request', $client);
+        $this->assertAttributeEquals(['this' => 'is a request test'], 'request', $client);
+    }
+
+    /**
+     * @dataProvider clients
      * @depends testSetRequest
      */
-    public function testGetRequest()
+    public function testGetRequest($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'getRequest'), 'método "getRequest" não encontrado');
-            $client->setRequest(['this' => 'is another request test']);
-            $request = $client->getRequest();
-            $this->assertNotEmpty($request);
-        });
-    }
-
-    public function testSetService()
-    {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'setService'), 'método "setService" não encontrado');
-            $client->setService('someService');
-            $this->assertObjectHasAttribute('service', $client);
-            $this->assertAttributeEquals('someService', 'service', $client);
-        });
+        $this->assertTrue(method_exists($client, 'getRequest'), 'método "getRequest" não encontrado');
+        $client->setRequest(['this' => 'is another request test']);
+        $request = $client->getRequest();
+        $this->assertNotEmpty($request);
     }
 
     /**
+     * @dataProvider clients
+     */
+    public function testSetService($client)
+    {
+        $this->assertTrue(method_exists($client, 'setService'), 'método "setService" não encontrado');
+        $client->setService('someService');
+        $this->assertObjectHasAttribute('service', $client);
+        $this->assertAttributeEquals('someService', 'service', $client);
+    }
+
+    /**
+     * @dataProvider clients
      * @depends testSetService
      */
-    public function testGetService()
+    public function testGetService($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'getService'), 'método "getService" não encontrado');
-            $client->setService('someService');
-            $service = $client->getService();
-            $this->assertNotEmpty($service);
-        });
-    }
-
-    public function testSetClient()
-    {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'setClient'), 'método "setClient" não encontrado');
-            $nativeCliente = new NativeSoapClient($client->getHost());
-            $client->setClient($nativeCliente);
-            $this->assertObjectHasAttribute('client', $client);
-            $this->assertAttributeEquals($nativeCliente, 'client', $client);
-        });
+        $this->assertTrue(method_exists($client, 'getService'), 'método "getService" não encontrado');
+        $client->setService('someService');
+        $service = $client->getService();
+        $this->assertNotEmpty($service);
     }
 
     /**
+     * @dataProvider clients
+     */
+    public function testSetClient($client)
+    {
+        $this->assertTrue(method_exists($client, 'setClient'), 'método "setClient" não encontrado');
+        $nativeCliente = new NativeSoapClient($client->getHost());
+        $client->setClient($nativeCliente);
+        $this->assertObjectHasAttribute('client', $client);
+        $this->assertAttributeEquals($nativeCliente, 'client', $client);
+    }
+
+    /**
+     * @dataProvider clients
      * @depends testSetClient
      */
-    public function testGetClient()
+    public function testGetClient($client)
     {
-        $clients = $this->clients();
-        $clients->each(function ($client) {
-            $this->assertTrue(method_exists($client, 'getClient'), 'método "getClient" não encontrado');
-            $nativeCliente = new NativeSoapClient($client->getHost());
-            $client->setClient($nativeCliente);
-            $nativeClient = $client->getClient();
-            $this->assertNotEmpty($nativeClient);
-        });
+        $this->assertTrue(method_exists($client, 'getClient'), 'método "getClient" não encontrado');
+        $nativeCliente = new NativeSoapClient($client->getHost());
+        $client->setClient($nativeCliente);
+        $nativeClient = $client->getClient();
+        $this->assertNotEmpty($nativeClient);
     }
 }
