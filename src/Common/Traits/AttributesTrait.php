73befe0e49627eacb7881ce21df95155f034f3f8
raivieira <rai.vieira@bludata.com.br>
2016-09-08 12:49:31 +0000

    Applied fixes from StyleCI

--- src/Common/Traits/AttributesTrait.php
+++ src/Common/Traits/AttributesTrait.php
@@ -23,6 +23,7 @@ trait AttributesTrait
                     if (method_exists($this, $getMethod)) {
                         return $this->$getMethod();
                     }
+
                     return $this->$attr;
                 },
                 $attributes
@@ -120,6 +121,7 @@ trait AttributesTrait
 
                     $newParam[] = $p;
                 }
+
                 return $newParam;
             }
 
