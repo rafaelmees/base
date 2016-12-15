1fff0abe007d1d1f7cb7cc72e681631fca20001e
raivieira <rai.vieira@bludata.com.br>
2016-12-15 18:02:53 +0000

    Apply fixes from StyleCI

--- src/Doctrine/Common/Traits/SetPropertiesEntityTrait.php
+++ src/Doctrine/Common/Traits/SetPropertiesEntityTrait.php
@@ -15,7 +15,7 @@ use ReflectionProperty;
 
 trait SetPropertiesEntityTrait
 {
-	public function setPropertiesEntity(array $data)
+    public function setPropertiesEntity(array $data)
     {
         foreach ($data as $key => $value) {
             $set = true;
@@ -31,9 +31,8 @@ trait SetPropertiesEntityTrait
             $methodSet = 'set'.ucfirst($key);
             $methodGet = 'get'.ucfirst($key);
 
-            if (method_exists($this, $methodSet) && $set)
-            {
-                /**
+            if (method_exists($this, $methodSet) && $set) {
+                /*
                  * Seta a propriedade com o valor enviado pelo usuário.
                  */
                 $this->$methodSet(is_string($value) && strlen($value) <= 0 ? null : $value);
@@ -55,42 +54,37 @@ trait SetPropertiesEntityTrait
                 /**
                  * Busca a anotação Doctrine\ORM\Mapping\Column.
                  */
-                $column = array_filter($propertyAnnotations, function ($annotation){
+                $column = array_filter($propertyAnnotations, function ($annotation) {
                     return $annotation instanceof Column;
                 });
 
-                if ($column)
-                {
+                if ($column) {
                     $column = $column[0];
                 }
 
                 /**
                  * Busca a anotação Bludata\Doctrine\Common\Annotations\ToObject.
                  */
-                $toObject = array_filter($propertyAnnotations, function ($annotation){
+                $toObject = array_filter($propertyAnnotations, function ($annotation) {
                     return $annotation instanceof ToObject;
                 });
 
-                /**
+                /*
                  * Verifica se a propriedade está usando a anotação Bludata\Doctrine\Common\Annotations\ToObject.
                  */
-                if ($toObject)
-                {
-                    /**
+                if ($toObject) {
+                    /*
                      * Caso seja um campo de data, utilizamos o método FormatHelper::parseDate para converter o valor enviado pelo usuário para um objeto DateTime.
                      */
-                    if ($column instanceof Column && ($column->type == 'date' || $column->type == 'datetime'))
-                    {
+                    if ($column instanceof Column && ($column->type == 'date' || $column->type == 'datetime')) {
                         $this->$methodSet(
                             FormatHelper::parseDate($valueKey)
                         );
-                    }
-                    else
-                    {
+                    } else {
                         /**
                          * Busca pelas anotações Doctrine\ORM\Mapping\ManyToOne || Doctrine\ORM\Mapping\OneToMany || Doctrine\ORM\Mapping\ManyToMany.
                          */
-                        $ormMapping = array_filter($propertyAnnotations, function ($annotation){
+                        $ormMapping = array_filter($propertyAnnotations, function ($annotation) {
                             return $annotation instanceof ManyToOne
                                    ||
                                    $annotation instanceof OneToMany
@@ -98,39 +92,33 @@ trait SetPropertiesEntityTrait
                                    $annotation instanceof ManyToMany;
                         });
 
-                        /**
+                        /*
                          * Se for encontrado alguma das anotações, iremos realizar o tratamento adequado para a anotação encontrada.
                          */
-                        if ($ormMapping)
-                        {
+                        if ($ormMapping) {
                             $ormMapping = $ormMapping[0];
 
                             $targetEntityName = $reflectionClass->getNamespaceName().'\\'.$ormMapping->targetEntity;
-                            $targetEntity = new $targetEntityName;
+                            $targetEntity = new $targetEntityName();
                             $repositoryTargetEntity = $targetEntity->getRepository();
 
-                            /**
+                            /*
                              * Se a propriedade estiver utilizando a anotação Doctrine\ORM\Mapping\ManyToOne e o usuário
                              * informou um número, então buscamos o devido objeto pelo seu id.
                              */
-                            if ($ormMapping instanceof ManyToOne && is_numeric($valueKey))
-                            {
+                            if ($ormMapping instanceof ManyToOne && is_numeric($valueKey)) {
                                 $this->$methodSet(
                                     $repositoryTargetEntity->find($valueKey)
                                 );
-                            }
-                            else if (($ormMapping instanceof OneToMany || $ormMapping instanceof ManyToMany) && is_array($valueKey))
-                            {
+                            } elseif (($ormMapping instanceof OneToMany || $ormMapping instanceof ManyToMany) && is_array($valueKey)) {
                                 /**
                                  * Caso a propriedade esteja utilizando as anotações Doctrine\ORM\Mapping\OneToMany || Doctrine\ORM\Mapping\ManyToMany,
                                  * então o usuário terá que implementar o método addX?().
                                  * Do contrário será lançada uma BadMethodCallException.
                                  */
                                 $methodAdd = 'add'.$ormMapping->targetEntity;
-                                if (!method_exists($this, $methodAdd))
-                                {
-                                    throw new \BadMethodCallException ('Para utilizar Bludata\Doctrine\Common\Annotations\ToObject em '.get_called_class().'::$'.$key.' você precisar declarar o método '.get_called_class().'::'.$methodAdd.'()');
-
+                                if (!method_exists($this, $methodAdd)) {
+                                    throw new \BadMethodCallException('Para utilizar Bludata\Doctrine\Common\Annotations\ToObject em '.get_called_class().'::$'.$key.' você precisar declarar o método '.get_called_class().'::'.$methodAdd.'()');
                                 }
 
                                 if (
@@ -143,8 +131,7 @@ trait SetPropertiesEntityTrait
                                 ) {
                                     $this->$methodSet(new ArrayCollection());
 
-                                    foreach ($valueKey as $value)
-                                    {
+                                    foreach ($valueKey as $value) {
                                         $this->$methodAdd(
                                             $ormMapping instanceof OneToMany ? $repositoryTargetEntity->findOrCreate($value) : $repositoryTargetEntity->find($value)
                                         );
--- src/Lumen/Providers/RegisterCustomAnnotationsServiceProvider.php
+++ src/Lumen/Providers/RegisterCustomAnnotationsServiceProvider.php
@@ -9,23 +9,19 @@ class RegisterCustomAnnotationsServiceProvider extends ServiceProvider
     public function register()
     {
         /**
-         * Caso haja outras annotations que necessitem ser registradas, basta adicionar o path das mesmas no array $paths
+         * Caso haja outras annotations que necessitem ser registradas, basta adicionar o path das mesmas no array $paths.
          */
         $paths = [
             __DIR__.'/../../Doctrine/Common/Annotations', //referente a Bludata\Doctrine\Common\Annotations
             base_path().'/vendor/symfony/validator/Constraints', //referente a Symfony\Component\Validator\Constraints
         ];
 
-        foreach ($paths as $path)
-        {
-            if ($handle = opendir($path))
-            {
-                while (false !== ($file = readdir($handle)))
-                {
+        foreach ($paths as $path) {
+            if ($handle = opendir($path)) {
+                while (false !== ($file = readdir($handle))) {
                     $pathFile = $path.'/'.$file;
 
-                    if (pathinfo($pathFile)['extension'] == 'php')
-                    {
+                    if (pathinfo($pathFile)['extension'] == 'php') {
                         \Doctrine\Common\Annotations\AnnotationRegistry::registerFile($pathFile);
                     }
                 }
