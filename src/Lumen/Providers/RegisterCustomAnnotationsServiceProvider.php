6abf174e035721bdb54a3a0783899f422edff76f
Rai Vieira <rai.vieira@bludata.local>
2017-03-29 17:38:32 -0300

    correcao

--- src/Lumen/Providers/RegisterCustomAnnotationsServiceProvider.php
+++ src/Lumen/Providers/RegisterCustomAnnotationsServiceProvider.php
@@ -8,26 +8,7 @@ class RegisterCustomAnnotationsServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        /**
-         * Caso haja outras annotations que necessitem ser registradas, basta adicionar o path das mesmas no array $paths.
-         */
-        $paths = [
-            __DIR__.'/../../Doctrine/Common/Annotations', //referente a Bludata\Doctrine\Common\Annotations
-            base_path().'/vendor/symfony/validator/Constraints', //referente a Symfony\Component\Validator\Constraints
-        ];
-
-        foreach ($paths as $path) {
-            if ($handle = opendir($path)) {
-                while (false !== ($file = readdir($handle))) {
-                    $pathFile = $path.'/'.$file;
-
-                    if (pathinfo($pathFile)['extension'] == 'php') {
-                        \Doctrine\Common\Annotations\AnnotationRegistry::registerFile($pathFile);
-                    }
-                }
-
-                closedir($handle);
-            }
-        }
+        register_annotation_dir(__DIR__.'/../../Doctrine/Common/Annotations');
+        register_annotation_dir(base_path().'/vendor/symfony/validator/Constraints');
     }
 }
