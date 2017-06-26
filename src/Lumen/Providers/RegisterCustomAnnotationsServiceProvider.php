8a10c5acfece3f314b1f3bf3de6285bad1395bbf
Raí Vieira Adriano <rai.vieira@bludata.com.br>
2017-06-26 14:12:47 -0300

    correcao

--- src/Lumen/Providers/RegisterCustomAnnotationsServiceProvider.php
+++ src/Lumen/Providers/RegisterCustomAnnotationsServiceProvider.php
@@ -8,6 +8,7 @@ class RegisterCustomAnnotationsServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        register_annotation_dir(__DIR__.'/../../Common/Annotations');
         register_annotation_dir(__DIR__.'/../../Common/Validators');
         register_annotation_dir(__DIR__.'/../../Doctrine/Common/Annotations');
         register_annotation_dir(base_path().'/vendor/symfony/validator/Constraints');
