0335b1729ff42012498bb64ee9c24c443c6fcc14
Raí Vieira Adriano <rai.vieira@bludata.com.br>
2017-12-19 17:06:08 -0200

    novo teste

--- src/Lumen/Traits/Tests/Http/Controllers/ReadTrait.php
+++ src/Lumen/Traits/Tests/Http/Controllers/ReadTrait.php
@@ -18,6 +18,20 @@ trait ReadTrait
         $this->assertNotNull($data[0]['id']);
     }
 
+    public function testCount()
+    {
+        $this->getServiceTest()->getRepositoryTest()->getFlushedMockObject();
+
+        $response = $this->curlHelper->setPosFixUrl('/count')->send()->getResponse();
+
+        $this->assertEquals(200, $response['code']);
+
+        $data = json_decode($response['data'], true);
+
+        $this->assertArrayHasKey('count', $data);
+        $this->assertGreaterThan(0, $data['count']);
+    }
+
     public function testShow()
     {
         $entity = $this->getServiceTest()->getRepositoryTest()->getFlushedMockObject();
